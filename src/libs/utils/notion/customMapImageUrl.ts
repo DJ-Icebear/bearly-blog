11a7d7885cee5ff1fe59602d6d610ab8bb454170
Björn <bjorn.helgeson@mindler.se>
2026-02-19 08:21:12 +0100

    Fix image URL:s

--- src/libs/utils/notion/customMapImageUrl.ts
+++ src/libs/utils/notion/customMapImageUrl.ts
@@ -1,6 +1,9 @@
 import { Block } from 'notion-types'
 
 export const customMapImageUrl = (url: string, block: Block): string => {
+  //@ts-ignore type definitions wrong
+  block = block.value
+
   if (!url) {
     throw new Error("URL can't be empty")
   }
